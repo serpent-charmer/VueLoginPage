0116d88f44d67bc06290e358dd5e7d5d463657b0
serpent-charmer <zagirov.robert@yandex.ru>
2026-02-05 19:27:05 +0500

    extracting component

--- src/components/account/AccountInfoComponent.vue
+++ src/components/account/AccountInfoComponent.vue
@@ -9,7 +9,7 @@ const props = defineProps({
     account: {
         type: AccountCredentials,
         required: true
-    }, 
+    },
     errors: {
         type: Map<String, String[]>,
         required: true
@@ -20,32 +20,43 @@ const isLdap = computed(() => props.account.type == AccountType.Ldap);
 const errors = computed(() => props.errors.get(props.account.uid));
 
 const updateAccount = (uid: String, obj: any) => {
-    emit('updateAccount', {uid, obj});
+    emit('updateAccount', { uid, obj });
 }
 const removeAccount = (uid: String) => {
-    emit('removeAccount', {uid});
+    emit('removeAccount', { uid });
 };
 </script>
 
 <template>
     <div :class="['account-row', isLdap ? 'account-ldap' : '']">
-        <sl-input :class="{ 'field-error': errors?.includes('tags') }"  :value="account.tagsText" @blur="updateAccount(account.uid, {tagsText: $event.target.value})"></sl-input>
-        <sl-select :value="account.type" @sl-change="updateAccount(account.uid, {type: $event.target.value})">
+        <sl-input :class="{ 'field-error': errors?.includes('tags') }" :value="account.tagsText"
+            @blur="updateAccount(account.uid, { tagsText: $event.target.value })"></sl-input>
+        <sl-select :value="account.type" @sl-change="updateAccount(account.uid, { type: $event.target.value })">
             <sl-option value="local">Локальная</sl-option>
             <sl-option value="ldap">LDAP</sl-option>
         </sl-select>
-        <sl-input :class="{ 'field-error': errors?.includes('login') }" :value="account.login" @blur="updateAccount(account.uid, {login: $event.target.value})"></sl-input>
-        <sl-input v-if="!isLdap" :class="{ 'field-error': errors?.includes('password') }" :value="account.password" type="password" password-toggle @blur="updateAccount(account.uid, {password: $event.target.value})"></sl-input>
+        <sl-input :class="{ 'field-error': errors?.includes('login') }" :value="account.login"
+            @blur="updateAccount(account.uid, { login: $event.target.value })"></sl-input>
+        <sl-input v-if="!isLdap" :class="{ 'field-error': errors?.includes('password') }" :value="account.password"
+            type="password" password-toggle
+            @blur="updateAccount(account.uid, { password: $event.target.value })"></sl-input>
         <sl-icon-button name="trash-fill" @click="removeAccount(account.uid)"></sl-icon-button>
     </div>
 
 </template>
 
 <style lang="scss" scoped>
-    .account-ldap {
-        grid-template-columns: 1fr 1fr 2fr 1fr;
-    }
-    .field-error {
-        --sl-input-background-color: #ffcccc;
-    }
+.account-row {
+    display: grid;
+    grid-template-columns: repeat(5, minmax(50px, 1fr));
+    align-items: center;
+}
+
+.account-ldap {
+    grid-template-columns: 1fr 1fr 2fr 1fr;
+}
+
+.field-error {
+    --sl-input-background-color: #ffcccc;
+}
 </style>
---
+++ src/components/page/LoginPage.vue
@@ -0,0 +1,85 @@
+<script setup lang="ts">
+import AccountInfo from '@/components/account/AccountInfoComponent.vue';
+import AccountType from '@/common/account/AccountType';
+import AccountCredentials from '@/common/account/AccountCredentials';
+
+import { useAccountStore } from '@/stores/AccountStore';
+
+const accountStore = useAccountStore();
+accountStore.copyFromDb();
+
+const addAccount = () => {
+  let account = new AccountCredentials(crypto.randomUUID(), "", AccountType.Local, "", null);
+  accountStore.add(account);
+};
+
+const updateAccount = (payload: any) => {
+  let { uid, obj } = payload;
+  accountStore.update(uid, obj);
+}
+
+const removeAccount = (payload: any) => {
+  let { uid } = payload;
+  accountStore.remove(uid);
+}
+
+
+</script>
+
+<template>
+  <div>
+
+    <div class="login-page">
+      <div class="header">
+
+        <span style="font-size: 2.5rem">Учетные записи</span>
+        <sl-icon-button name="plus-lg" label="Edit" style="font-size: 2.5rem;" @click="addAccount"></sl-icon-button>
+
+      </div>
+
+      <sl-alert open>
+        <sl-icon slot="icon" name="info-circle"></sl-icon>
+        Для указания нескольких меток для одной пары логин/пароль используйте разделитель ;
+      </sl-alert>
+
+
+      <div>
+        <div class="account-row">
+          <span>Метки</span>
+          <span>Тип записи</span>
+          <span>Логин</span>
+          <span>Пароль</span>
+        </div>
+        <AccountInfo v-for="(account) in accountStore.accounts" :account="account" :errors="accountStore.errors" :key="account.uid"
+          @update-account="updateAccount" @remove-account="removeAccount" />
+      </div>
+
+    </div>
+
+  </div>
+</template>
+
+<style lang="scss">
+@import '@fontsource/ibm-plex-mono/500.css';
+@import '@fontsource/ibm-plex-mono/600.css';
+@import '@fontsource/ibm-plex-mono/700.css';
+
+.login-page {
+  font-family: 'IBM Plex Mono';
+}
+
+.header {
+  display: flex;
+}
+
+.account-row {
+  display: grid;
+  grid-template-columns: repeat(5, minmax(50px, 1fr));
+  align-items: center;
+}
+
+.account-row>* {
+  margin-right: 1rem;
+  margin-bottom: .5rem;
+}
+</style>
